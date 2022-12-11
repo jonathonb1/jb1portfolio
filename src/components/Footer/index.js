53122ba9f5cf61e3b0d16f64e49002be1c680e6c
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-12-10 22:08:55 -0800

    adding homepage info and styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import GithubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
         <GithubIcon />
+        <LinkedInIcon />
       </div>
-      <p> Made with love by Jonathon Biala</p>
+      <p> &copy; 2022 Made with love by Jonathon Biala</p>
     </div>
   );
 }
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,39 +3,41 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "../styles/Home.css";
-import Illustration from "../assets/cooks.jpg";
 
 function AboutMe() {
   return (
     <div className="home">
-      <Container>
-        <Row className="about-app">
-          <Col sm={8}>
-            <div className="intro">
-              <h1>
-                {" "}
-                {/* What is <span className="only-cooks"> OnlyCooks </span>?{" "} */}
-              </h1>
-              <div className="my-2">
-                {/* <p>
-                  Only Cooks is a social platform where you can store your most
-                  treasured recipes privately or share with friends. If you are
-                  looking for a new and cool recipe to impress your loved ones,
-                  you can explore our database of recipes from kitchens across
-                  the globe! All are welcome to join this community of cooks and
-                  creators!
-                </p>
-                <img
-                  src={Illustration}
-                  alt="two cooks"
-                  className="justify-content-md-center" */}
-                />
-              </div>
-            </div>
-          </Col>
-          <Col className="sm={4}"></Col>
-        </Row>
-      </Container>
+      <div className="intro">
+        <h1>
+          Who <span className="heading"> am I </span>?{" "}
+        </h1>
+        <div className="my-2">
+          <p>
+            My name is Jonathon and am a software developer who is new in this
+            space. I enjoy the process of learning and creating things that dont
+            exist!
+          </p>
+        </div>
+      </div>
+      <div className="skills">
+        <h1>
+          S<span className="heading">kills</span>
+        </h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>HTML, CSS, BootStrap, Bulma, ReactJS, Git, NPM </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span></span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span></span>
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
